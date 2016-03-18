62c18ce0f1fc8878d3a5532b6775ba13f91e8127
Marko Stevankovic <markostevankovic95@gmail.com>
2016-03-18 20:22:44 +0100

    New branch interfaceimplementation created. Inside interfaceimplementation branch all methods of ExchangeOfficeInterface interface implemented. Constructor in class ExchangeRate changed. Signatures of all methods in interface ExchangeOfficeInterface changed.

--- ExchangeOffice/src/domain/ExchangeOffice.java
+++ ExchangeOffice/src/domain/ExchangeOffice.java
@@ -1,26 +1,58 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import exchange_office_interface.EchangeOfficeInterface;
 
 public class ExchangeOffice implements EchangeOfficeInterface
 {
+	ArrayList<Currency> currencies = new ArrayList<Currency>();
 
 	@Override
-	public void addNewExchangeRate(Date date, double buyingEchangeRate, double sellingExchangeRate)
-	{
+	public void addNewExchangeRate(Currency currency, Date date, double buyingEchangeRate, double sellingExchangeRate) 
+	{		
+		for(Currency temporaryCurrency : currencies)
+			if(temporaryCurrency.equals(currency))
+			{
+				temporaryCurrency.getExchangeRate().add(new ExchangeRate(date, buyingEchangeRate, sellingExchangeRate));
+				return;
+			}
+		
+			throw new RuntimeException("Given currency not found!");
 	}
 
 	@Override
-	public void removeGivenExchangeRate(Date date) 
-	{	
+	public void removeGivenExchangeRate(Currency currency, Date date) 
+	{		
+		for(Currency temporaryCurrency : currencies)
+		{
+			if(temporaryCurrency.equals(currency))
+			{
+				for(ExchangeRate er : temporaryCurrency.getExchangeRate())
+				{
+					if(er.getDate().equals(date))
+					{
+						temporaryCurrency.getExchangeRate().remove(er);
+						return;
+					}
+				}
+			}
+		}
+		
+		throw new RuntimeException("Given currency with specific date not found!");
 	}
 
 	@Override
-	public ExchangeRate getExchangeRate(Date date)
-	{
-		return null;
+	public ExchangeRate getExchangeRate(Currency currency, Date date)
+	{		
+		for(Currency temporaryCurrency : currencies)
+			if(temporaryCurrency.equals(currency))
+				for(ExchangeRate temporaryExchangeRate : temporaryCurrency.getExchangeRate())
+					if(temporaryExchangeRate.getDate().equals(date))
+						return temporaryExchangeRate;
+		
+		throw new RuntimeException("Given currency with given date not found!");
 	}
 
 }
